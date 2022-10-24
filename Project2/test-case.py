774e672d33bd150d6b2351fe75ce658c995b9b49
msethi006 <m.sethi006@gmail.com>
2022-10-24 11:33:29 +0530

    Added test case code

---
+++ Project2/test-case.py
@@ -0,0 +1,8 @@
+import requests
+headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'}
+s = requests.session()
+page_res = s.get('http://18.191.246.46:8501/', headers = headers)
+if page_res.status_code == 200:
+    print("WEbsite is working fine")
+else:
+    print('Website is not working')
