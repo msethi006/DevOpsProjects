09ea7d8dd9a6eaa8518f034e421d287b8c3ca443
msethi006 <m.sethi006@gmail.com>
2022-10-26 06:30:46 +0530

    Modifed JenkinsFile

--- Project2/test-case.py
+++ Project2/test-case.py
@@ -1,8 +1,9 @@
 import requests
 headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'}
 s = requests.session()
-page_res = s.get('http://18.191.246.46:8501/', headers = headers)
-if page_res.status_code == 200:
-    print("WEbsite is working fine",flush= True)
-else:
+try:
+    page_res = s.get('http://18.222.67.133:8501/', headers = headers)
+    if page_res.status_code == 200:
+        print("WEbsite is working fine",flush= True)
+except:
     print('Website is not working',flush= True)
